17c331d81212fea2161b2aa0c86276efe245a116
Ivan <ivan0313@users.noreply.github.com>
2020-04-17 00:42:34 +0800

    Create main.cpp
    Created main.cpp with some comments on what code should be included in the file.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // Print Starting Screen.
+
+    // Ask player if they want to start new game, load an old save or view previous game's results.
+
+    // Go through rounds in a loop until game goal is reached.
+
+    // End credits, view game results and/or loop back to the starting Screen.
+
+    return 0;
+}
