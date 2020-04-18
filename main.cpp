8e93a3f6e3ffd81188dc30fac58c854313671f68
Ivan <ivan0313@users.noreply.github.com>
2020-04-18 14:02:08 +0800

    Created function for loading country stats
    Created function for loading country stats and empty function to calculate the player's overall performance index, and put them in seperated files, also created header files for them

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 
-bool function_to_prove_Ivan_is_stupid()
-{
-  ;
-}
 
 int main()
 {
-  // This is my comment, Mike is stupid.
-
   return 0;
-
+}
---
+++ performance_index.cpp
@@ -0,0 +1,7 @@
+#include "performance_index.h"
+
+float calculate_overall_performance_index(float pi_by_country[], struct country AllCountries[])
+{
+  float overall_pi=0;
+  return overall_pi;
+}
