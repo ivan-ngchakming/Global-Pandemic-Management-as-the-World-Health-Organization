c1f1e19292b656338717c910edbe84ec144f2b7d
Cheng Mo Shun <h3568882@academy11.cs.hku.hk>
2020-04-22 10:38:56 +0800

    I dont know why my account is not using mikecheng18

---
+++ dynamic_array.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+struct country(){
+  string name;
+  double population;
+  double death_rate,recovery_rate;economic_impact;
+  double PI;
+};
+//for storing the data for a country
+
+void grow_array( )
--- main.cpp
+++ main.cpp
@@ -1,6 +1,78 @@
 #include <iostream>
+#include <string>
+#include <conio.h>
+//for clear screen
+using namespace std;
+
+struct Node{
+  string content;
+  Node * next;
+};
+//linked list
+
+struct country
+{
+  string name;
+  double population;
+  double infections;
+  double deaths;
+  double recovered;
+  double economy;
+  double pi;
+  //pi: performance index
+};
+//for storing the data for a country
+
+struct WHO{
+  int capital;
+  //stores the capital that the WHO have
+  int staff;
+  //stores the no. of staff that WHO has
+  int medical;
+  //stores amout of medical equipment that WHO has
+}
+
+void printruler(){
+  cout<<"--------------------------------------------------"<<endl;
+  //synchronized every ruller have the same length
+}
 
 int main()
 {
-  return 0;
+  //------------------initialization-----------------------------
+
+  Node * deck_head = new Node;
+  deck_head=NULL;
+  //initialize deck list and it is empty
+  Node * trash_head = new Node;
+  trash_head=NULL;
+  //initialize trash list and it is empty
+  int day=0;
+  //day stores the virtual time in the game
+  //also mean the number of round the player had played
+
+  //------------------initialization-----------------------------
+
+  //------------------New game? ----------------------------
+  string s="0";
+  while ((s!="1")&&(s!="2")){
+    cout<<"1. New game"<<endl;
+    cout<<"2. Load game"<<endl;
+    printruller();
+    cin>>s;
+  }
+  if (s=="1"){
+    //new game
+  }
+  else{
+    //load game
+  }
+  //------------------------------New game? ------------------------------
+
+  //---------------------------the game ----------------------------------
+
+  while calculate_overall_performance_index(AllCountries[]);
+
+  //---------------------------the game ----------------------------------
+
 }
